2e361967cd27e1e000dc6c95fa0b10d26071e1a3
Jon <JJKing619@gmail.com>
2022-02-17 10:36:47 -0500

    created methods

--- FirstGroupProject/Program.cs
+++ FirstGroupProject/Program.cs
@@ -1,85 +1,82 @@
 ﻿
 using FirstGroupProject;
-bool runProgram = true;
-Menu menu = new Menu();
 
+bool runProgram = true;
+bool checkOut = true;
+int customerItemChoice = 0;
+int itemQuantityChoice = 0;
+Menu menu = new Menu();
+List<Product> cart = new List<Product>();
 Console.WriteLine("Welcome to Lucas & Jon's game shop!");
 Console.WriteLine();
 
 while (runProgram)
 {
-    //showing menu (should let them choose by number)
-    menu.ListProducts();
-
-    Console.WriteLine();
-    Console.Write("Enter a number to select an item: ");
-
-    //Let user choose item
-    int customerItemChoice = 0;
-    while (!int.TryParse(Console.ReadLine(), out customerItemChoice))
+    while (checkOut)
     {
-        Console.Write("Not a valid option. Please try again: ");
-    }
-
-    if (customerItemChoice >= 1 && customerItemChoice <= menu.Products.Count())
-    {
-        Console.Write("How many would you like to purchase?: ");
-
-        //user chooses quantity of item 
-        int itemQuantityChoice = 0;
-        while (!int.TryParse(Console.ReadLine(), out itemQuantityChoice))
+        //showing menu (should let them choose by number)
+        menu.ListProducts();
+        ChooseItem();
+        if (customerItemChoice >= 1 && customerItemChoice <= menu.Products.Count())
         {
-            Console.Write("Not a valid option. Please try again: ");
+            ChooseQuantity();   
+            //Adds to cart & displays Line Total
+            AddToCart();
         }
-        //display Line Total
-        Product chosenProduct = menu.Products[customerItemChoice - 1];
-
-        //TODO: get cart to not reset each loop
-        List<Product> cart = new List<Product>();
-        for(int i = 0; i < itemQuantityChoice; i++)
+        else
         {
-            cart.Add(chosenProduct);
-        }             
-                                                                                        
-        //cart.Add(chosenProduct);
-        foreach (Product i in cart)
-        {
-            Console.WriteLine(i);
+            Console.WriteLine("Sorry that was not a valid input");
         }
-         
-        decimal customerLineTotal = itemQuantityChoice * chosenProduct.Price;
-        Console.WriteLine($"No Problem! That will be: {customerLineTotal}");
-        decimal customerOrderTotal = 0m;
-        //TODO: calculate customer total from cart
-        customerOrderTotal = cart.Sum(product => product.Price);
-        Console.WriteLine($"Your order total is: {customerOrderTotal}");
+       checkOut = Validator.Validator.GetContinue("Would you like to add more items (y) or checkout (n)?");
+
     }
-    else
-    {
-        Console.WriteLine("Sorry that was not a valid input");
-    }
-    runProgram = Validator.Validator.GetContinue("Would you like to continue? y/n: ");
+    decimal customerOrderTotal = 0m;
+    DisplayCart();
+    //Calculating and displaying cart total
+    customerOrderTotal = cart.Sum(product => product.Price);
+    Console.WriteLine($"Your order total is: {customerOrderTotal}");
+    runProgram = Validator.Validator.GetContinue("Would you like to continue? y/n: ");//TODO: fix loop to rerun program & not just keep showing total
     Console.WriteLine();
 }
 
 //methods
-//static int ChooseItem(string x)
-//{
-//    int customerItemChoice = 0;
-//    while (!int.TryParse(Console.ReadLine(), out customerItemChoice))
-//    {
-//        Console.Write("Not a valid option. Please try again: ");
-//    }
-//    return customerItemChoice;
-//}
 
+int ChooseItem()
+{
+    Console.WriteLine();
+    Console.Write("Enter a number to select an item: ");
+    while (!int.TryParse(Console.ReadLine(), out customerItemChoice))
+    {
+        Console.Write("Not a valid option. Please try again: ");
+    }
+    return customerItemChoice;
+}
 
+int ChooseQuantity()
+{
+    Console.Write("How many would you like to purchase?: ");
+    while (!int.TryParse(Console.ReadLine(), out itemQuantityChoice))
+    {
+        Console.Write("Not a valid option. Please try again: ");
+    }
+    return itemQuantityChoice;
+}
 
-//void DisplayCart()
-//{
-//    List<Product> cart = new List<Product>();
-//    for (int i = 0; i < cart.Count; i++)
-//    {
-//        Console.WriteLine($"{i + 1} {cart[i].ToString()}");//need to format eventually
-//    }
-//}
+void DisplayCart()
+{
+    foreach (Product i in cart)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+void AddToCart()
+{
+    Product chosenProduct = menu.Products[customerItemChoice - 1];
+    for (int i = 0; i < itemQuantityChoice; i++)
+    {
+        cart.Add(chosenProduct);
+    }
+    decimal customerLineTotal = itemQuantityChoice * chosenProduct.Price;
+    Console.WriteLine($"{customerLineTotal} will be added to your cart.");
+}
